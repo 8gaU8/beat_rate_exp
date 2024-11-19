276e716effa760c39f98ede3864f4de75951d641
8gaU8 <haga.yuya2.2016@gmail.com>
2024-11-19 23:27:24 +0200

    不要なファイルの削除

--- src/init.js
+++
@@ -1,8 +0,0 @@
-// import { initJsPsych } from 'jspsych'
-
-// export const jsPsych = initJsPsych({
-//     auto_preload: true,
-// })
-// export const {randomization} = jsPsych.randomization
-
-// export default {jsPsych, randomization}
